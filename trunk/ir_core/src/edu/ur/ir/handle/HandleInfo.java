be846d5a4fbdc994f1c9197d1cda8836bd2b815a
natesarr <natesarr@76f36ee2-a05b-11dd-91bf-632da8b6e10b>
2009-03-02 15:42:35 +0000

    initial add of handle basic code

---
+++ trunk/ir_core/src/edu/ur/ir/handle/HandleInfo.java
@@ -0,0 +1,229 @@
+package edu.ur.ir.handle;
+
+import edu.ur.persistent.BasePersistent;
+
+/**
+ * Class that represents handle information.
+ * 
+ * @author Nathan Sarr
+ *
+ */
+public class HandleInfo extends BasePersistent{
+	
+	/** eclipse generated id */
+	private static final long serialVersionUID = -4297910642983562791L;
+	
+	/** the main data type used for handles in this system */
+	public static final String URL_DATA_TYPE = "URL";
+	
+	/** 24 hours - default time to live */
+	public static final Integer DEFAULT_TIME_TO_LIVE = new Integer(86400);
+	
+	/**  relative time to live */
+	public static final Integer RELATIVE_TIME_TO_LIVE_TYPE = new Integer(0);
+	
+	/** time stamp value  */
+	public static final Integer DEFAULT_TIMESTAMP_VALUE = new Integer(0);
+	
+	/** default references */
+	public static final String DEFAULT_REFERENCES = "";
+	
+	/** default admin read */
+	public static final boolean DEFAULT_ADMIN_READ = true;
+	
+	/** default public read */
+	public static final boolean DEFAULT_PUBLIC_READ = true;
+	
+	/** default admin write */
+	public static final boolean DEFAULT_ADMIN_WRITE = true;
+	
+	/** default public write */
+	public static final boolean DEFAULT_PUBLIC_WRITE = false;
+	
+	/** index - positive integer value - this should be unique across the local naming authority */
+	private Long index;
+	
+	/** the type of data for the handle data */
+	private String dataType = URL_DATA_TYPE;
+	
+	/** data for the handle - will usually be a URL */
+	private String data;
+	
+	/** time to live type */
+	private Integer timeToLiveType = RELATIVE_TIME_TO_LIVE_TYPE;
+	
+	/** time to live */
+	private Integer timeToLive = DEFAULT_TIME_TO_LIVE;
+	
+	/** time stamp value */
+	private Integer timestamp = DEFAULT_TIMESTAMP_VALUE;
+	
+	/** references for this handle */
+	private String references = DEFAULT_REFERENCES;
+	
+	/** admin read permissions */
+	private boolean adminRead = DEFAULT_ADMIN_READ;
+	
+	/** admin write permissions */
+	private boolean adminWrite = DEFAULT_ADMIN_WRITE;
+	
+	/** public read permissions */
+	private boolean publicRead = DEFAULT_PUBLIC_READ;
+	
+	/** public write permissions */
+	private boolean publicWrite = DEFAULT_PUBLIC_WRITE;
+	
+	/** name authority for this handle */
+	private NameAuthority nameAuthority;
+	
+
+	/**
+	 * Package protected constructor
+	 */
+	HandleInfo(){}
+	
+	/**
+	 * Public constructor for handle.
+	 * 
+	 * @param index - unique index for the naming authority
+	 * @param data - data for the handle
+	 * @param nameAuthority - name authority for this handle
+	 */
+	public HandleInfo(Long index, String data, NameAuthority nameAuthority)
+	{
+	   setId(index);
+	   setData(data);
+	   setNameAuthority(nameAuthority);
+	}
+	
+	
+	public Long getIndex() {
+		return index;
+	}
+	public void setIndex(Long index) {
+		this.index = index;
+	}
+	public String getDataType() {
+		return dataType;
+	}
+	public void setDataType(String dataType) {
+		this.dataType = dataType;
+	}
+	public String getData() {
+		return data;
+	}
+	public void setData(String data) {
+		this.data = data;
+	}
+	public Integer getTimeToLiveType() {
+		return timeToLiveType;
+	}
+	public void setTimeToLiveType(Integer timeToLiveType) {
+		this.timeToLiveType = timeToLiveType;
+	}
+	public Integer getTimeToLive() {
+		return timeToLive;
+	}
+	public void setTimeToLive(Integer timeToLive) {
+		this.timeToLive = timeToLive;
+	}
+	public Integer getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(Integer timestamp) {
+		this.timestamp = timestamp;
+	}
+	public String getReferences() {
+		return references;
+	}
+	public void setReferences(String references) {
+		this.references = references;
+	}
+	public boolean isAdminRead() {
+		return adminRead;
+	}
+	public void setAdminRead(boolean adminRead) {
+		this.adminRead = adminRead;
+	}
+	public boolean isAdminWrite() {
+		return adminWrite;
+	}
+	public void setAdminWrite(boolean adminWrite) {
+		this.adminWrite = adminWrite;
+	}
+	public boolean isPublicRead() {
+		return publicRead;
+	}
+	public void setPublicRead(boolean publicRead) {
+		this.publicRead = publicRead;
+	}
+	public boolean isPublicWrite() {
+		return publicWrite;
+	}
+	public void setPublicWrite(boolean publicWrite) {
+		this.publicWrite = publicWrite;
+	}
+	
+	public NameAuthority getNameAuthority() {
+		return nameAuthority;
+	}
+
+	public void setNameAuthority(NameAuthority nameAuthority) {
+		this.nameAuthority = nameAuthority;
+	}
+
+	public int hashCode()
+	{
+		int value = 0;
+		value += nameAuthority == null ? 0 : nameAuthority.hashCode();
+		value += index == null ? 0 : index.hashCode();
+		return value;
+	}
+	
+	
+	public boolean equals(Object o)
+	{
+		if (this == o) return true;
+		if (!(o instanceof HandleInfo)) return false;
+
+		final HandleInfo other = (HandleInfo) o;
+		if( (index != null && !index.equals(other.getIndex()) ) ||
+			(index == null && other.getIndex() != null ) ) return false;
+		
+		if( (nameAuthority != null && !nameAuthority.equals(other.getNameAuthority()) ) ||
+			(nameAuthority == null && other.getNameAuthority() != null ) ) return false;
+	
+		return true;		
+	}
+	
+	public String toString()
+	{
+		StringBuffer sb = new StringBuffer("[ id ");
+		sb.append(id);
+		sb.append(" index = ");
+		sb.append(index);
+		sb.append(" dataType = ");
+		sb.append(dataType);
+		sb.append(" data = ");
+		sb.append(data);
+		sb.append(" timeToLiveType = ");
+		sb.append(timeToLiveType);
+		sb.append(" timeToLive = ");
+		sb.append(timeToLive);
+		sb.append(" timestamp= ");
+		sb.append(timestamp);
+		sb.append(" references = ");
+		sb.append(references);
+		sb.append(" adminRead = ");
+		sb.append(adminRead);
+		sb.append(" adminWrite ");
+		sb.append(adminWrite);
+		sb.append(" publicRead = ");
+		sb.append(publicRead);
+		sb.append(" publicWrite = ");
+		sb.append(publicWrite);
+		sb.append("]");
+		return sb.toString();
+	}
+
+}
---
+++ trunk/ir_core/src/edu/ur/ir/handle/NameAuthority.java
@@ -0,0 +1,104 @@
+package edu.ur.ir.handle;
+
+import edu.ur.persistent.BasePersistent;
+
+/**
+ * This class represents the naming authority including the local name
+ * 
+ * @author NathanS
+ *
+ */
+public class NameAuthority extends BasePersistent{
+
+	/** eclipse generated id  */
+	private static final long serialVersionUID = 5099575658284711364L;
+	
+	/** Name representing the naming authority */
+	private String namingAuthority;
+	
+	/** local handle name */
+	private String localName;
+	
+
+	/**
+	 * Package protected constructor
+	 */
+	NameAuthority(){}
+	
+	/**
+	 * Create a handle name authority
+	 * 
+	 * @param handleNamingAuthority
+	 * @param handleLocalName
+	 */
+	public NameAuthority(String namingAuthority, String localName)
+	{
+		this.setLocalName(localName);
+		this.setNamingAuthority(namingAuthority);
+	}
+
+	public String getNamingAuthority() {
+		return namingAuthority;
+	}
+
+	public void setNamingAuthority(String handleNamingAuthority) {
+		this.namingAuthority = handleNamingAuthority;
+	}
+	
+	public String getLocalName() {
+		return localName;
+	}
+
+	public void setLocalName(String handleLocalName) {
+		this.localName = handleLocalName;
+	}
+	
+	/**
+	 * Returns the full name authority
+	 *  e.g. 0.NA/12345 where
+	 *  0.NA is the naming authority and 12345 is the local name
+	 * @return
+	 */
+	public String getFullNameAuthority()
+	{
+		return namingAuthority + "/" + localName;
+	}
+	
+	public boolean equals(Object o)
+	{
+		if (this == o) return true;
+		if (!(o instanceof NameAuthority)) return false;
+
+		final NameAuthority other = (NameAuthority) o;
+		if( (namingAuthority != null && !namingAuthority.equals(other.getNamingAuthority()) ) ||
+			(namingAuthority == null && other.getNamingAuthority() != null ) ) return false;
+		
+		if( (localName != null && !localName.equals(other.getLocalName()) ) ||
+			(localName == null && other.getLocalName() != null ) ) return false;
+		
+		return true;
+	}
+	
+	public int hashCode()
+	{
+		int value = 0;
+		value += namingAuthority == null ? 0 : namingAuthority.hashCode();
+		value += localName == null ? 0 : localName.hashCode();
+		return value;
+	}
+
+	public String toString()
+	{
+		StringBuffer sb = new StringBuffer("[ id = ");
+		sb.append(id);
+		sb.append(" namingAuthority = ");
+		sb.append( namingAuthority);
+		sb.append(" localName = ");
+		sb.append(localName);
+		sb.append("]");
+		return sb.toString();
+	}
+	
+	
+
+}
