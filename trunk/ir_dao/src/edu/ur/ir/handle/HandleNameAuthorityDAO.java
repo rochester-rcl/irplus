d9ca8b88b76ae4b03cb5fbd34bd95dc775003d02
natesarr <natesarr@76f36ee2-a05b-11dd-91bf-632da8b6e10b>
2009-03-02 21:40:40 +0000

    initial add of handle name authority DAO

---
+++ trunk/ir_dao/src/edu/ur/ir/handle/HandleNameAuthorityDAO.java
@@ -0,0 +1,13 @@
+package edu.ur.ir.handle;
+
+import edu.ur.dao.CountableDAO;
+import edu.ur.dao.CrudDAO;
+
+
+
+/**
+ * Data access for name authority 
+ * @author Nathan Sarr
+ *
+ */
+public interface HandleNameAuthorityDAO extends CountableDAO, CrudDAO<HandleNameAuthority>{}
