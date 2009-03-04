d07c7251f8cf0b09c488dfca5382b9797f2718e0
natesarr <natesarr@76f36ee2-a05b-11dd-91bf-632da8b6e10b>
2009-03-04 13:48:50 +0000

    updated to make name more descriptive

---
+++ trunk/ir_core/src/edu/ur/ir/handle/NextUniqueHandleName.java
@@ -0,0 +1,20 @@
+package edu.ur.ir.handle;
+
+/**
+ * This interface is guaranteed to return the next unique handle 
+ * value so long as it is the only object used to return
+ * values.
+ * 
+ * @author Nathan Sarr
+ *
+ */
+public interface NextUniqueHandleName {
+	
+	/**
+	 * Gets the next unique handle name.
+	 * 
+	 * @return next unique handle name
+	 */
+	public String nextName();
+
+}
