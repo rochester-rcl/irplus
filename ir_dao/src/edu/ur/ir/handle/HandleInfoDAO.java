f3f6148a2791e82b902e0cf90bc1813289421031
natesarr <natesarr@76f36ee2-a05b-11dd-91bf-632da8b6e10b>
2009-03-04 13:44:19 +0000

    updates for handle code persistence

---
+++ ir_dao/src/edu/ur/ir/handle/HandleInfoDAO.java
@@ -0,0 +1,12 @@
+package edu.ur.ir.handle;
+
+import edu.ur.dao.CountableDAO;
+import edu.ur.dao.CrudDAO;
+
+/**
+ * DAO interface for accessing handle information.
+ * 
+ * @author Nathan Sarr
+ *
+ */
+public interface HandleInfoDAO extends CountableDAO, CrudDAO<HandleInfo>{}
