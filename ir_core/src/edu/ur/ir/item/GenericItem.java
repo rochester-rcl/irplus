21c893e92716a41abb4059c03faa19812337a162
natesarr <natesarr@76f36ee2-a05b-11dd-91bf-632da8b6e10b>
2009-03-10 18:35:30 +0000

    moved handle information to institutional item versions

--- ir_core/src/edu/ur/ir/item/GenericItem.java
+++ ir_core/src/edu/ur/ir/item/GenericItem.java
@@ -27,7 +27,6 @@ import java.util.Set;
 import org.apache.log4j.Logger;
 
 import edu.ur.ir.file.IrFile;
-import edu.ur.ir.handle.HandleInfo;
 import edu.ur.ir.item.ItemFile;
 import edu.ur.ir.item.ItemLink;
 import edu.ur.ir.item.ItemObject;
@@ -126,8 +125,7 @@ public class GenericItem extends CommonPersistent implements Cloneable {
 	/** Indicates that this item can be viewed by the public. */
 	private boolean publiclyViewable = true;
 	
-	/** handle information for the item  */
-	private HandleInfo handleInfo;
+
 	
 	/**
 	 * Default constructor; 
@@ -1618,12 +1616,5 @@ public class GenericItem extends CommonPersistent implements Cloneable {
 		this.releaseDate = releaseDate;
 	}
 	
-	public HandleInfo getHandleInfo() {
-		return handleInfo;
-	}
-
-	public void setHandleInfo(HandleInfo handleInfo) {
-		this.handleInfo = handleInfo;
-	}
 
 }
