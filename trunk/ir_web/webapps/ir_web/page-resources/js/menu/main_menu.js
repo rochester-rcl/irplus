89822e05f2f42b724c1e1f5d7449a3c446d4c691
natesarr <natesarr@76f36ee2-a05b-11dd-91bf-632da8b6e10b>
2009-04-22 15:28:03 +0000

    updated to not auto display the data

--- trunk/ir_web/webapps/ir_web/page-resources/js/menu/main_menu.js
+++ trunk/ir_web/webapps/ir_web/page-resources/js/menu/main_menu.js
@@ -18,7 +18,7 @@
   // Initialize and render the menu bar when it is available in the DOM
   YAHOO.util.Event.onContentReady("mainMenu", function () {
       // Instantiate and render the menu bar
-      var oMenuBar = new YAHOO.widget.MenuBar("mainMenu", { autosubmenudisplay: true, hidedelay: 750, lazyload: true });
+      var oMenuBar = new YAHOO.widget.MenuBar("mainMenu", { autosubmenudisplay: false, hidedelay: 750, lazyload: true });
       /*
           Call the "render" method with no arguments since the markup for 
           this menu already exists in the DOM.
