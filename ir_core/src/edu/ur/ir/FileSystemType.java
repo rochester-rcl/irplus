1e60745b8622af807a98585734cd5165fb4c69c6
natesarr <natesarr@76f36ee2-a05b-11dd-91bf-632da8b6e10b>
2009-06-18 15:31:11 +0000

    updated to use orderable interface and comparators

--- ir_core/src/edu/ur/ir/FileSystemType.java
+++ ir_core/src/edu/ur/ir/FileSystemType.java
@@ -18,47 +18,53 @@ package edu.ur.ir;
 
 import java.io.Serializable;
 
+import edu.ur.order.Orderable;
+
 /**
  * Allows the setting of different file types.
  * 
  * @author Nathan Sarr
  *
  */
-public class FileSystemType implements Serializable{
+public class FileSystemType implements Serializable, Orderable{
 	
 	/** eclipse genreated id */
 	private static final long serialVersionUID = -7485498621126903978L;
 
 	/** Represents a file type */
-	public static final FileSystemType FILE = new FileSystemType("file");
-	public static final FileSystemType FOLDER = new FileSystemType("folder");
-	public static final FileSystemType PERSONAL_FILE = new FileSystemType("personalFile");
-	public static final FileSystemType SHARED_INBOX_FILE = new FileSystemType("sharedInboxFile");
-	public static final FileSystemType PERSONAL_FOLDER = new FileSystemType("personalFolder");
-	public static final FileSystemType ITEM = new FileSystemType("item");
-	public static final FileSystemType COLLECTION = new FileSystemType("collection");
-	public static final FileSystemType PERSONAL_ITEM = new FileSystemType("personalItem");
-	public static final FileSystemType PERSONAL_COLLECTION = new FileSystemType("personalCollection");
-	public static final FileSystemType INSTITUTIONAL_ITEM = new FileSystemType("institutionalItem");
-	public static final FileSystemType INSTITUTIONAL_COLLECTION = new FileSystemType("institutionalCollection");
-	public static final FileSystemType RESEARCHER_FILE = new FileSystemType("researcherFile");
-	public static final FileSystemType RESEARCHER_FOLDER = new FileSystemType("researcherFolder");
-	public static final FileSystemType RESEARCHER_PUBLICATION = new FileSystemType("researcherPublication");
-	public static final FileSystemType RESEARCHER_LINK = new FileSystemType("researcherLink");
-	public static final FileSystemType RESEARCHER_INSTITUTIONAL_ITEM = new FileSystemType("researcherInstitutionalItem");
+	public static final FileSystemType FILE = new FileSystemType("file", 1);
+	public static final FileSystemType FOLDER = new FileSystemType("folder", 10);
+	public static final FileSystemType PERSONAL_FILE = new FileSystemType("personalFile", 1);
+	public static final FileSystemType SHARED_INBOX_FILE = new FileSystemType("sharedInboxFile", 1);
+	public static final FileSystemType PERSONAL_FOLDER = new FileSystemType("personalFolder", 10);
+	public static final FileSystemType ITEM = new FileSystemType("item", 1);
+	public static final FileSystemType COLLECTION = new FileSystemType("collection", 10);
+	public static final FileSystemType PERSONAL_ITEM = new FileSystemType("personalItem", 1);
+	public static final FileSystemType PERSONAL_COLLECTION = new FileSystemType("personalCollection", 10);
+	public static final FileSystemType INSTITUTIONAL_ITEM = new FileSystemType("institutionalItem", 1);
+	public static final FileSystemType INSTITUTIONAL_COLLECTION = new FileSystemType("institutionalCollection", 10);
+	public static final FileSystemType RESEARCHER_FILE = new FileSystemType("researcherFile", 1);
+	public static final FileSystemType RESEARCHER_FOLDER = new FileSystemType("researcherFolder", 10);
+	public static final FileSystemType RESEARCHER_PUBLICATION = new FileSystemType("researcherPublication", 1);
+	public static final FileSystemType RESEARCHER_LINK = new FileSystemType("researcherLink", 1);
+	public static final FileSystemType RESEARCHER_INSTITUTIONAL_ITEM = new FileSystemType("researcherInstitutionalItem", 1);
 
 	
 	/** The type of object */
 	private String type;
+	
+	/**  ordering for type */
+	private int order;
 	
 	/**
 	 * Default constructor
 	 * 
 	 * @param type
 	 */
-	private FileSystemType(String type)
+	private FileSystemType(String type, int order)
 	{
 		this.type = type;
+		this.order = order;
 	}
 	
 	/**
@@ -69,6 +75,16 @@ public class FileSystemType implements Serializable{
 	public String getType()
 	{
 		return this.type;
+	}
+	
+	/**
+	 * Order basis for the type.
+	 * 
+	 * @return
+	 */
+	public int getOrder()
+	{
+		return order;
 	}
 	
 	public String toString()
