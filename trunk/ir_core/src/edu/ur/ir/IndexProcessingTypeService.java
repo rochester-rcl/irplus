b4561c79e6d9045205d13fc352f1f6d5a338643d
natesarr <natesarr@76f36ee2-a05b-11dd-91bf-632da8b6e10b>
2009-06-20 12:59:04 +0000

    updated with initial add of index processing

---
+++ trunk/ir_core/src/edu/ur/ir/IndexProcessingTypeService.java
@@ -0,0 +1,75 @@
+/**  
+   Copyright 2008 University of Rochester
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/  
+
+
+package edu.ur.ir;
+
+import java.util.List;
+
+/**
+ * Interface to deal with index processing types.
+ * 
+ * @author Nathan Sarr
+ *
+ */
+public interface IndexProcessingTypeService {
+	
+    /**
+     * Get a count of index processing types
+     *  
+     * @return - the number of index processing types found
+     */
+    public Long getCount();
+    
+    /**
+     * Delete the index processing type 
+     * 
+     * @param  indexProcessingType
+     */
+    public void delete(IndexProcessingType indexProcessingType);    
+    
+    /**
+     * Get an index processing type by name.
+     * 
+     * @param name - name of the copyright statement.
+     * @return - the found index processing type or null if the index processint type is not found.
+     */
+    public IndexProcessingType get(String name);
+    
+    /**
+     * Get an index processing type by id
+     * 
+     * @param id - unique id of the copyright statement.
+     * @param lock - upgrade the lock on the data
+     * @return - the found copyright statement or null if the copyright statement is not found.
+     */
+    public IndexProcessingType get(Long id, boolean lock);
+    
+    /**
+     * Save the index processing type
+     * 
+     * @param IndexProcessingType
+     */
+    public void save(IndexProcessingType indexProcessingType);
+ 
+	/**
+	 * Get all index processing types
+	 * 
+	 * @return List of all index processing types
+	 */
+	public List<IndexProcessingType> getAll();
+	
+}
