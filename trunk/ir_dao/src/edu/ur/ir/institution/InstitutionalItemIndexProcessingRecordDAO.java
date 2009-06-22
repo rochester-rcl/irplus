e549a5078c0ddeaf257a4b2c5025ae43622d128b
natesarr <natesarr@76f36ee2-a05b-11dd-91bf-632da8b6e10b>
2009-06-22 02:28:06 +0000

    updated for institutional item index processing record

---
+++ trunk/ir_dao/src/edu/ur/ir/institution/InstitutionalItemIndexProcessingRecordDAO.java
@@ -0,0 +1,40 @@
+/**  
+   Copyright 2008 University of Rochester
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/  
+
+package edu.ur.ir.institution;
+
+import java.util.List;
+
+import edu.ur.dao.CountableDAO;
+import edu.ur.dao.CrudDAO;
+
+/**
+ * Data access for institutional item index processing record.
+ * 
+ * @author Nathan Sarr
+ *
+ */
+public interface InstitutionalItemIndexProcessingRecordDAO 
+    extends CrudDAO<InstitutionalItemIndexProcessingRecord>, CountableDAO{
+	
+	/**
+	 * Get all institutional item indexing processing record ordered by item id and updated date.
+	 * 
+	 * @return all institutional item index processing records or an empty list if none found
+	 */
+	public List<InstitutionalItemIndexProcessingRecord> getAllOrderByItemIdUpdatedDate();
+
+}
