5f263e55156746744a1f770cb2ae6cf74a1ae433
natesarr <natesarr@76f36ee2-a05b-11dd-91bf-632da8b6e10b>
2009-07-14 17:11:48 +0000

    updated for name comparator

---
+++ ur_common_util/src/edu/ur/simple/type/DescendingNameComparator.java
@@ -0,0 +1,22 @@
+package edu.ur.simple.type;
+
+import java.io.Serializable;
+import java.util.Comparator;
+
+/**
+ * Return the name aware objects in descending order.
+ * 
+ * @author Nathan Sarr
+ *
+ */
+public class DescendingNameComparator implements Comparator<NameAware>, Serializable{
+
+	/** eclipse genarated id */
+	private static final long serialVersionUID = -2166294396178883394L;
+
+	@Override
+	public int compare(NameAware o1, NameAware o2) {
+		return o2.getName().compareToIgnoreCase((o1.getName()));
+	}
+
+}
