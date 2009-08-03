5a41de0f8ff4939dc4b3b8083f17cac84c8808b7
natesarr <natesarr@76f36ee2-a05b-11dd-91bf-632da8b6e10b>
2009-08-03 15:50:16 +0000

    updated to set repository license on reviewable items

--- trunk/ir_web/src/edu/ur/ir/web/action/item/ViewReviewableItems.java
+++ trunk/ir_web/src/edu/ur/ir/web/action/item/ViewReviewableItems.java
@@ -22,13 +22,21 @@ import java.util.List;
 import com.opensymphony.xwork2.ActionSupport;
 
 import edu.ur.ir.NoIndexFoundException;
+import edu.ur.ir.handle.HandleInfo;
+import edu.ur.ir.handle.HandleNameAuthority;
+import edu.ur.ir.handle.UniqueHandleNameGenerator;
 import edu.ur.ir.index.IndexProcessingType;
 import edu.ur.ir.index.IndexProcessingTypeService;
 import edu.ur.ir.institution.InstitutionalItem;
 import edu.ur.ir.institution.InstitutionalItemIndexProcessingRecordService;
+import edu.ur.ir.institution.InstitutionalItemService;
+import edu.ur.ir.institution.InstitutionalItemVersion;
 import edu.ur.ir.institution.ReviewableItem;
 import edu.ur.ir.institution.ReviewableItemService;
+import edu.ur.ir.institution.service.InstitutionalItemVersionUrlGenerator;
 import edu.ur.ir.item.ItemObject;
+import edu.ur.ir.repository.Repository;
+import edu.ur.ir.repository.RepositoryService;
 import edu.ur.ir.user.IrUser;
 import edu.ur.ir.user.UserService;
 import edu.ur.ir.web.action.UserIdAware;
@@ -51,9 +59,18 @@ public class ViewReviewableItems  extends ActionSupport implements UserIdAware {
 	
 	/** service for marking items that need to be indexed */
 	private InstitutionalItemIndexProcessingRecordService institutionalItemIndexProcessingRecordService;
+	
+	/** Institutional item service */
+	private InstitutionalItemService institutionalItemService;
 
 	/** index processing type service */
 	private IndexProcessingTypeService indexProcessingTypeService;
+	
+	/** generates unique handle names */
+	private UniqueHandleNameGenerator uniqueHandleNameGenerator;
+	
+	/** url generator for institutional items. */
+	private InstitutionalItemVersionUrlGenerator institutionalItemVersionUrlGenerator;
 	
 	/**  Get the logger for this class */
 	private static final Logger log = Logger.getLogger(ViewReviewableItems.class);
@@ -77,6 +94,9 @@ public class ViewReviewableItems  extends ActionSupport implements UserIdAware {
 	
 	/** Reason for rejection */
 	private String reason;
+	
+	/**  Repository information data access  */
+	private RepositoryService repositoryService;
 
 	/**
 	 * View all pending items
@@ -114,9 +134,36 @@ public class ViewReviewableItems  extends ActionSupport implements UserIdAware {
 	 */
 	public String accept() throws NoIndexFoundException {
 		IrUser user = userService.getUser(userId, false);
+		HandleInfo info = null;
 		
 		reviewableItem = reviewableItemService.getReviewableItem(reviewableItemId, false);
 		InstitutionalItem institutionalItem = reviewableItemService.acceptItem(reviewableItem, user); 
+		Repository repository = repositoryService.getRepository(Repository.DEFAULT_REPOSITORY_ID, false);
+		
+		// set the default license if one exists for the repository
+		if( repository.getDefaultLicense() != null)
+		{
+		    institutionalItem.getVersionedInstitutionalItem().getCurrentVersion().addRepositoryLicense(repository.getDefaultLicense(), user);
+		}
+		
+		// save the item 
+		institutionalItemService.saveInstitutionalItem(institutionalItem);
+		
+		// add a handle if the handle service is available
+		HandleNameAuthority handleNameAuthority = repository.getDefaultHandleNameAuthority();
+		if( handleNameAuthority != null)
+		{
+			String nextHandleName = uniqueHandleNameGenerator.nextName();
+		    InstitutionalItemVersion itemVersion = institutionalItem.getVersionedInstitutionalItem().getCurrentVersion();
+			
+			String url = institutionalItemVersionUrlGenerator.createUrl(institutionalItem, itemVersion.getVersionNumber());
+			info = new HandleInfo(nextHandleName, url, handleNameAuthority);
+			
+		    itemVersion.setHandleInfo(info);
+		    
+		    // save the item with the new handle information.
+			institutionalItemService.saveInstitutionalItem(institutionalItem);
+		}
 		
 		reviewableItemService.sendItemAcceptanceEmail(reviewableItem);
 
@@ -206,6 +253,25 @@ public class ViewReviewableItems  extends ActionSupport implements UserIdAware {
 	public void setIndexProcessingTypeService(
 			IndexProcessingTypeService indexProcessingTypeService) {
 		this.indexProcessingTypeService = indexProcessingTypeService;
+	}
+
+	public void setUniqueHandleNameGenerator(
+			UniqueHandleNameGenerator uniqueHandleNameGenerator) {
+		this.uniqueHandleNameGenerator = uniqueHandleNameGenerator;
+	}
+
+	public void setInstitutionalItemVersionUrlGenerator(
+			InstitutionalItemVersionUrlGenerator institutionalItemVersionUrlGenerator) {
+		this.institutionalItemVersionUrlGenerator = institutionalItemVersionUrlGenerator;
+	}
+
+	public void setRepositoryService(RepositoryService repositoryService) {
+		this.repositoryService = repositoryService;
+	}
+
+	public void setInstitutionalItemService(
+			InstitutionalItemService institutionalItemService) {
+		this.institutionalItemService = institutionalItemService;
 	}
 
 }
