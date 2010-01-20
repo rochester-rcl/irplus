f189684c9784f7e25d487912d451a67d18603b8b
natesarr <natesarr@76f36ee2-a05b-11dd-91bf-632da8b6e10b>
2010-01-20 21:17:48 +0000

    inital add

---
+++ ir_service/src/edu/ur/ir/user/service/FakeLdapUserService.java
@@ -0,0 +1,70 @@
+/**  
+   Copyright 2008-2010 University of Rochester
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/  
+
+
+package edu.ur.ir.user.service;
+
+import org.springframework.ldap.core.DirContextAdapter;
+import org.springframework.ldap.core.DirContextOperations;
+import org.springframework.security.GrantedAuthority;
+import org.springframework.security.userdetails.UserDetails;
+
+import edu.ur.ir.user.LdapUserService;
+import edu.ur.ir.user.UserService;
+
+/**
+ * This class always returns a null dirContext operations.  This can be
+ * used when you don't have a system user or when you cannot anonymously 
+ * bind to the ldap directory.
+ * 
+ * @author Nathan Sarr
+ *
+ */
+public class FakeLdapUserService implements LdapUserService  {
+
+	/** loads user data from local db*/
+	private UserService userService;
+	
+	/**
+	 * This actually does not search but always returns null.
+	 * 
+	 * @see edu.ur.ir.user.LdapUserService#searchForUser(java.lang.String)
+	 */
+	public DirContextOperations searchForUser(String ldapUserName) {
+		return null;
+	}
+
+	public UserDetails mapUserFromContext(DirContextOperations arg0,
+			String username, GrantedAuthority[] arg2) {
+		return userService.getUserByLdapUserName(username);
+	}
+
+	public void mapUserToContext(UserDetails arg0, DirContextAdapter arg1) {
+		// do nothing
+	}
+
+	public UserService getUserService() {
+		return userService;
+	}
+
+	public void setUserService(UserService userService) {
+		this.userService = userService;
+	}
+
+
+
+
+}
