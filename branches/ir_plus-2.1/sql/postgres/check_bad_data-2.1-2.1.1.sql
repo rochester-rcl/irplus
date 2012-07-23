d6d87edcfc4f70c45a28721f1561bbddd7a257ec
natesarr <natesarr@76f36ee2-a05b-11dd-91bf-632da8b6e10b>
2012-07-23 16:37:38 +0000

    updates for checking bad email addresses and upgrading to new tables

--- branches/ir_plus-2.1/sql/postgres/check_bad_data-2.1-2.1.1.sql
+++ branches/ir_plus-2.1/sql/postgres/check_bad_data-2.1-2.1.1.sql
@@ -1,6 +1,17 @@
+-- Find bad file shares
+
 select * 
 from ir_user.personal_file pf1
 where exists( select * from ir_user.personal_file pf2
 where pf2.user_id = pf1.user_id
 and pf2.versioned_file_id = pf1.versioned_file_id
 and pf2.personal_file_id != pf1.personal_file_id)
+
+
+-- Find bad emails
+
+select *
+from ir_user.user_email email1
+where exists( select * from ir_user.user_email email2
+where email2.user_email_id != email1.user_email_id
+and lower(email2.email) = lower(email1.email));
